04ad8b864997d2efdd2b45fe5c374700788b0904
Alessandra <alexxandraoix.3@alumnos.cei.es>
2025-01-15 00:13:35 +0100

    actualizo headers

--- index.js
+++ index.js
@@ -18,29 +18,37 @@ app.use(express.urlencoded({extended:false}))
 //     next();
 // })
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/', (req, res) => {
     res.json('Haciendo get en /');
 })
 
 app.use(router)
 
-const corsOption = {
-    origin: ['http://localhost:3000'],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}
+let corsOptions = {
+    origin: '*'
+};
 
-app.use(cors(corsOption));
-
-app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "React app URL"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
+corsOptions()
 
 // Middlewares para gestionar errores
 app.use(( req , res , next  )=>{
