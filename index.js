ce7e4647b6da07e6f229cc4984e382aaaef924dc
Alessandra <alexxandraoix.3@alumnos.cei.es>
2025-01-14 23:36:32 +0100

    actualizo headers en index.js para politicas cors

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/', (req, res) => {
 
 app.use(router)
 
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "React app URL"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+
 // Middlewares para gestionar errores
 app.use(( req , res , next  )=>{
     let error = new Error()
