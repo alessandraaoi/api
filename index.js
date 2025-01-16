4c4aafbb932fcc36ae204023df7d06d5ac543a84
Alessandra <alexxandraoix.3@alumnos.cei.es>
2025-01-16 20:22:54 +0100

    ok

--- index.js
+++ index.js
@@ -13,40 +13,36 @@ const app = express ()
 app.use(cors())
 app.use (express.json())
 app.use(express.urlencoded({extended:false}))
-// app.use((req, res, next) =>  {
-//     res.setHeader("Access-Control-Allow-Private-Network", "true")
-//     next();
-// })
+
+
+const { User } = require('../models/usuarios');
+
 
 // Add headers before the routes are defined
-app.use( (req, res, next) => {
+// app.use( (req, res, next) => {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 
-let corsOptions = {
-    origin: 'http://localhost:3000'
-};
+app.get('/', async (req, res) => {
 
-corsOptions()
-
-app.get('/', (req, res) => {
-    res.json('Haciendo get en /');
+    const buscar = await User.find()
+    res.json(buscar);
 })
 
 app.use(router)
